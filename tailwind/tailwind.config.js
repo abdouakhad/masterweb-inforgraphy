a6fc892451333cf4d770788601265314b3a89cff
Abdoul Akhad Top <akhad0015@gmail.com>
2021-08-20 21:55:35 +0000

    first commit

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        'jaune':'#FFD230',
+        'bleu':'#01BDFE'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
